3712035afc4a60e057f907bb55c7b8c584091f4f
Devkans <argevorgyan@griddynamics.com>
2025-12-17 03:04:05 +0400

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 1.5.0"
-  
+
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -9,8 +9,8 @@ terraform {
   }
 
   backend "gcs" {
-    bucket = "spring-petclinic-tfe-ar"
-    prefix = "terraform/state"
+    bucket = "spring-petclinic-tf-state"
+    prefix = "infra"
   }
 }
 
@@ -20,8 +20,6 @@ provider "google" {
   zone    = var.zone
 }
 
-
-# NETWORK
 resource "google_compute_network" "vpc" {
   name                    = var.network
   auto_create_subnetworks = false
@@ -34,22 +32,33 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc.id
 }
 
-# DATABASE 
+resource "google_compute_firewall" "allow_ssh_http" {
+  name    = "petclinic-fw"
+  network = google_compute_network.vpc.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
 resource "google_sql_database_instance" "postgres" {
   name             = "petclinic-db"
   database_version = "POSTGRES_15"
   region           = var.region
-  
+
   settings {
     tier = "db-f1-micro"
-    
+
     ip_configuration {
       ipv4_enabled    = false
       private_network = google_compute_network.vpc.id
     }
   }
 
-  deletion_protection = false # Set to true for production
+  deletion_protection = false
 }
 
 resource "google_sql_database" "database" {
@@ -63,7 +72,6 @@ resource "google_sql_user" "user" {
   password = var.postgres_password
 }
 
-# COMPUTE ENGINE VM
 resource "google_compute_address" "static_ip" {
   name = var.ip
 }
@@ -72,8 +80,7 @@ resource "google_compute_instance" "vm" {
   name         = var.vm
   machine_type = "e2-medium"
   zone         = var.zone
-
-  tags = ["petclinic-vm"]
+  tags         = ["petclinic-vm"]
 
   boot_disk {
     initialize_params {
@@ -91,7 +98,6 @@ resource "google_compute_instance" "vm" {
     }
   }
 
-  # Using your terraform-sa service account
   service_account {
     email  = "terraform-ci@gd-gcp-internship-devops.iam.gserviceaccount.com"
     scopes = ["cloud-platform"]
@@ -99,7 +105,6 @@ resource "google_compute_instance" "vm" {
 
   metadata_startup_script = <<-EOF
     #!/bin/bash
-    # Docker installation script
     apt-get update
     apt-get install -y docker.io docker-compose
     systemctl start docker
@@ -109,14 +114,12 @@ resource "google_compute_instance" "vm" {
   EOF
 }
 
-# ARTIFACT REGISTRY
 resource "google_artifact_registry_repository" "repo" {
   location      = var.region
   repository_id = var.repo
   format        = "DOCKER"
 }
 
-# OUTPUTS
 output "application_url" {
   value = "http://${google_compute_address.static_ip.address}:${var.port}"
 }
@@ -124,5 +127,3 @@ output "application_url" {
 output "database_private_ip" {
   value = google_sql_database_instance.postgres.private_ip_address
 }
-
-
---
+++ terraform/providers.tf
@@ -0,0 +1,6 @@
+terraform {
+  backend "gcs" {
+    bucket = "spring-petclinic-tf-state"
+    prefix = "infra"
+  }
+}
